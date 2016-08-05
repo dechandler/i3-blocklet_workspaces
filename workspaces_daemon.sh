4b956f4b66ad5859e1914ba6df3c63106ba6100f
David Chandler <dechandler10@gmail.com>
2016-08-05 14:24:15 -0500

    it didnt like my ~

--- workspaces_daemon.sh
+++ workspaces_daemon.sh
@@ -6,12 +6,12 @@
 # subscribes to i3 window and workspace events and
 # updates a file that i3blocks cats as a blocklet
 #
-_i3subscribe="~/.i3/i3_blocklets/i3subscribe.pl"
-_workspaces_gen="~/.i3/i3_blocklets/workspaces_gen.py"
+_i3subscribe="/home/david/.i3/i3_blocklets/i3subscribe.pl"
+_workspaces_gen="/home/david/.i3/i3_blocklets/workspaces_gen.py"
 _dump_file="/dev/shm/workspaces.pango"
 
-$_i3subscribe window workspace | \
-while read -r _event; do
+$_i3subscribe window workspace \
+| while read -r _event; do
     grep 'focus$' >/dev/null <<< $_event \
     || $_workspaces_gen "$@" > $_dump_file;
 done
